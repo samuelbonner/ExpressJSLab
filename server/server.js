a6bda56b0cc724ac54871850c259457ef10cd598
Samuel Bonner <bonner.sam@gmail.com>
2021-07-13 13:42:05 -0500

    Started Advanced section. It brokenly works.

--- server/server.js
+++ server/server.js
@@ -1,13 +1,21 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 let app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-    console.log(`The requested URL was ${req.originalUrl}`);
-    next();
+  console.log(`The requested URL was ${req.originalUrl}`);
+  next();
 });
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.post("/contact-form", (req, res) => {
+  console.log(req.body.text);
+  res.send(`Thank you for submitting your form! ${"\n"} ${req.body.text}`);
+});
 
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.listen(3000);
