9660b72def5c50c396ba78f0f0e126d57d567c5e
Samuel Bonner <bonner.sam@gmail.com>
2021-07-13 14:48:57 -0500

    Removed bodyParser (deprecated) and changed to express.urlencoded

--- server/server.js
+++ server/server.js
@@ -1,21 +1,31 @@
 const express = require("express");
 const path = require("path");
-const fs = require("fs");
+const fs = require("fs"); // Currently unused
 let app = express();
-const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false })) // parses form data
 
-app.use((req, res, next) => {
-  console.log(`The requested URL was ${req.originalUrl}`);
-  next();
-});
+
+// This console logs the request original Url & request url when serving client
+// app.use((req, res, next) => {
+//   console.log(`The requested Original URL was ${req.originalUrl}`);
+//   console.log(`The requested URL was ${req.url}`);
+//   console.log(); // blank for readability
+//   next();
+// });
 
 app.post("/contact-form", (req, res) => {
+const newMessage = {
+    email: req.body.email,
+    message: req.body.message
+}
+
   console.log(req.body.text);
-  res.send(`Thank you for submitting your form! ${"\n"} ${req.body.text}`);
+  res.send(`Thank you for submitting your form! ${"\n"}
+  Email: ${newMessage.email}
+  Message: ${newMessage.message}`);
 });
 
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static("public"));
 
 app.listen(3000);
